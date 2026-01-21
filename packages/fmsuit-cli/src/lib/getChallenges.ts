3cdd5e7fad1197bc7387636482da0aaac9dfcf8e
Eze-sk <ezequielsmiak@gmail.com>
2026-01-21 15:41:17 -0300

    feat: update getChallenges function to accept status parameter for improved state management

--- packages/fmsuit-cli/src/lib/getChallenges.ts
+++ packages/fmsuit-cli/src/lib/getChallenges.ts
@@ -1,16 +1,25 @@
-import { CACHE_FILE } from "../consts/env";
-import type { ChallengeData } from "../typings/challengeData";
-import { getValidCache } from "../utils/getValidCache";
-import { scraping } from "./scraping";
+import { CACHE_FILE } from '../consts/env'
+import type { ChallengeData } from '../typings/challengeData'
+import { getValidCache } from '../utils/getValidCache'
+import { scraping } from './scraping'
+import type { Dispatch, SetStateAction } from 'react'
+import type { Status } from '../hooks/useInitialization'
+
+interface Props {
+  status: Dispatch<SetStateAction<Status>>
+}
 
 /**
  * Retrieves challenge data from cache or by scraping the Frontend Mentor website.
+ * @param {Props} props - Function parameters
+ * @param {Dispatch<SetStateAction<Status>>} props.status - State setter for updating the current operation status
  * @returns {Promise<ChallengeData>} An object containing challenge data
  */
-export async function getChallenges(): Promise<ChallengeData> {
+export async function getChallenges({ status }: Props): Promise<ChallengeData> {
   const cacheChallenges = await getValidCache(CACHE_FILE)
 
   if (!cacheChallenges) {
+    status('scraping_data')
     return await scraping()
   }
 
