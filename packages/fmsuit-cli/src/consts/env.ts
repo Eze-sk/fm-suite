f4adcb8661bab9e54b2220879d0039f6a48743b4
Eze-sk <ezequielsmiak@gmail.com>
2026-02-24 17:15:04 -0300

    fix: update DATA_DIR path and add CHALLENGES_FILES_DIR constant

--- packages/fmsuit-cli/src/consts/env.ts
+++ packages/fmsuit-cli/src/consts/env.ts
@@ -6,11 +6,14 @@ import {
   type Browser,
 } from '@utils/findChromiumBasedBrowser'
 
-export const DATA_DIR = join(homedir(), '.fmsuit-cli-sessions')
+export const DATA_DIR = join(homedir(), '.fmsuit-cli')
 export const CACHE_FILE = join(DATA_DIR, 'challenge_cache.json')
 export const SESSION_FILE = join(DATA_DIR, 'fm-session.json')
+export const CHALLENGES_FILES_DIR = join(DATA_DIR, 'challenges_files')
 
 const browser = await defaultBrowser()
 export const BROWSER_PATH = findChromiumBasedBrowser(browser.name as Browser)
 
 export const FM_URL = 'https://www.frontendmentor.io'
+
+export const CONFIG_FILE_NAME = "fm-cli"
