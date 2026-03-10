567c8a5ca357387d93ae5078c48e95940020a57a
Eze-sk <ezequielsmiak@gmail.com>
2026-03-10 11:39:10 -0300

    feat: enhance AppStore with download step management and updateDownloadStep method

--- packages/fmsuit-cli/src/stores/useApp.ts
+++ packages/fmsuit-cli/src/stores/useApp.ts
@@ -14,11 +14,21 @@ export type AppStatus =
   | 'completed'
   | 'error'
 
+export type DownloadStepStatus = 'pending' | 'loading' | 'completed'
+
+export interface DownloadStep {
+  id: string;
+  label: string;
+  status: DownloadStepStatus;
+}
+
 export interface AppStore {
   appStatus: AppStatus
+  downloadStep: DownloadStep[]
   data: ChallengeData | null
   config: ConfigFile
   setStatus: (status: AppStatus) => void
+  updateDownloadStep: (id: string, status: DownloadStepStatus) => void
   setData: (data: ChallengeData | null) => void
   updateData: () => Promise<void>
   setConfig: (data: ConfigFile) => void
@@ -30,11 +40,23 @@ export interface AppStore {
  */
 export const useAppStore = create<AppStore>((set) => ({
   appStatus: "idle",
+  downloadStep: [
+    { id: 'DOWNLOAD', label: 'Download challenge', status: 'pending' },
+    { id: 'BUILD', label: 'Starting the challenge', status: 'pending' },
+    { id: 'MERGE', label: 'Merge resource', status: 'pending' },
+  ],
   data: null,
   config: {
     challengePath: process.cwd()
   },
   setStatus: (status): void => set({ appStatus: status }),
+  updateDownloadStep: (id, status): void => {
+    set((state) => ({
+      downloadStep: state.downloadStep.map((s) =>
+        s.id === id ? { ...s, status } : s
+      ),
+    }))
+  },
   setData: (data): void => set({ data }),
   updateData: async (): Promise<void> => {
     try {
